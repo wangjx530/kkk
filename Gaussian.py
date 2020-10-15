80e084ef14e376c505f3c71dfb33327bacb6fc42
wangjx530 <1152559156@qq.com>
2020-10-15 15:52:00 +0800

    G2

---
+++ Gaussian.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def draw_gauss():
+    x = np.linspace(-6, 6)
+    sigma = 1
+    mu = 0
+    y = (
+            1
+            / ((2 * np.pi) ** 0.5 * sigma)
+            * np.e ** (-(x - mu) ** 2 / (2 * np.pi * sigma ** 2))
+    )
+    plt.plot(x, y)
+    plt.show()
+
+
+draw_gauss()
