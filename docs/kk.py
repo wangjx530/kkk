8c20cb761c0b053e83be9926fb12716f716b72fa
wangjx530 <1152559156@qq.com>
2020-10-17 15:55:52 +0800

    feature

---
+++ docs/kk.py
@@ -0,0 +1,9 @@
+fruits = ["apple", "mango", "orange"]               #list列表 每个值都可以通过索引进行标识
+numbers = (1, 2, 3)                                 #tuple 元组 元组的元素不能修改
+alphabets = {'a':'apple', 'b':'ball', 'c':'cat'}    #dictionary 字典“键值对”
+vowels = {'a', 'e', 'i' , 'o', 'u'}                 #set 集合跟数学上的集合是一致的，不允许有重复元素，而且可以进行交集、并集、差集等运算。
+
+print(fruits)
+print(numbers)
+print(alphabets)
+print(vowels)
